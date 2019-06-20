0d0e7d7398bfc7cec055049387b7a2efda647363
austie702 <austie702@gmail.com>
2019-06-20 03:21:22 -0500

    complete function-conversion Q02

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -9,10 +9,14 @@ console.log('* * * Function-Conversion Block -- START * * *');
 // myFunction();
 const myFunction = () => {console.log('Function was invoked!')}
 myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = (param) => param;
+anotherFunction('Example');
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
